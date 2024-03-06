24d675aa58d74a345f961829e0645fd2ea4b598f
Radhia007 <145629268+Radhia007@users.noreply.github.com>
2024-03-06 13:12:29 +0700

    Add files via upload

---
+++ kuliah/pertemuan3.php
@@ -0,0 +1,20 @@
+<?php 
+
+$a = 9;
+$b = 4;
+
+$luas_a = sa*sa*sa*;
+$luas_b = sb*sb*sb*;
+
+funcition hitung_luas_kubus(sisi_kubus) {
+$hasil = $sisi_kubus*$sisi_kubus*$sisi_kubus*;
+return $luas;
+}
+
+echo($luas_a+$luas_b);
+
+echo("<br/>");
+$luas_kubus_1 = hitung_luas_kubus($a);
+$luas_kubus_2 = hitung_luas_kubus($b);
+echo($luas_kubus_1+$luas_kubus_2);
+
